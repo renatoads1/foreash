70670720baf3dddd29efb300c93da16c779040e4
Renato Azevedo <renatoads1@gmail.com>
2020-04-09 10:04:49 -0300

    frist commit by renato

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace foreashlendoarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("for");
+            string[] vet = new string[] {"Renato","Isabela","Julia" };
+            for (int i = 0; i < vet.Length; i++) {
+                Console.WriteLine(vet[i]);
+            }
+            Console.WriteLine("foreash");
+            foreach (string obj in vet) {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
